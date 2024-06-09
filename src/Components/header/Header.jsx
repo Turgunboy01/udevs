cff581d9b3cf6c3110cf927623a04b280f113c74
Turgunboy <turgunboy.channel@gmail.com>
2024-06-09 18:02:40 +0500

    upg

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -10,6 +10,9 @@ import ser2 from "../../../public/header/ser2.svg";
 import ser3 from "../../../public/header/ser3.svg";
 import ser4 from "../../../public/header/ser4.svg";
 import ser5 from "../../../public/header/ser5.svg";
+import rus from "../../../public/header/rus.svg";
+import eng from "../../../public/header/eng.svg";
+// import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 
 const Header = () => {
   return (
@@ -66,7 +69,7 @@ const Header = () => {
                   smooth={true}
                   duration={500}
                   style={{ border: "none" }}
-                  className="text-[14px] px-8 border-b-[2px] border-b-[#1b59f700] hover:bg-[#1b5bf7] py-2  cursor-pointer flex items-center gap-4"
+                  className="text-[14px] px-8 border-b-[2px] border-b-[#1b59f700] hover:bg-[#1b5bf7] py-2 hover:text-[#fff]  cursor-pointer flex items-center gap-4"
                 >
                   <img src={ser1} alt="" />
                   Development of mobile applications
@@ -78,7 +81,7 @@ const Header = () => {
                   style={{ border: "none" }}
                   smooth={true}
                   duration={500}
-                  className="text-[14px] px-8 border-b-[2px] border-b-[#1b59f700] hover:bg-[#1b5bf7] py-2  cursor-pointer flex items-center gap-4"
+                  className="text-[14px] px-8 border-b-[2px] border-b-[#1b59f700] hover:text-[#fff] hover:bg-[#1b5bf7] py-2  cursor-pointer flex items-center gap-4"
                 >
                   <img src={ser2} alt="" />
                   Development and implementation ERP systems
@@ -90,7 +93,7 @@ const Header = () => {
                   smooth={true}
                   duration={500}
                   style={{ border: "none" }}
-                  className="text-[14px] px-8 border-b-[2px] border-b-[#1b59f700] hover:bg-[#1b5bf7] py-2  cursor-pointer flex items-center gap-4"
+                  className="text-[14px] px-8 border-b-[2px] border-b-[#1b59f700] hover:bg-[#1b5bf7] py-2  hover:text-[#fff] cursor-pointer flex items-center gap-4"
                 >
                   <img src={ser3} alt="" />
                   User interface, User experience design
@@ -102,7 +105,7 @@ const Header = () => {
                   smooth={true}
                   duration={500}
                   style={{ border: "none" }}
-                  className="text-[14px] px-8 border-b-[2px] border-b-[#1b59f700] hover:bg-[#1b5bf7] py-2 font-bold cursor-pointer flex items-center gap-4"
+                  className="text-[14px] px-8 border-b-[2px] border-b-[#1b59f700] hover:text-[#fff] hover:bg-[#1b5bf7] py-2  cursor-pointer flex items-center gap-4"
                 >
                   <img src={ser4} alt="" />
                   IT consulting
@@ -114,7 +117,7 @@ const Header = () => {
                   spy={true}
                   smooth={true}
                   duration={500}
-                  className="text-[14px] px-8 border-b-[2px] border-b-[#1b59f700] hover:bg-[#1b5bf7] py-2 font-bold cursor-pointer flex items-center gap-4"
+                  className="text-[14px] px-8 border-b-[2px] border-b-[#1b59f700] hover:text-[#fff] hover:bg-[#1b5bf7] py-2  cursor-pointer flex items-center gap-4"
                 >
                   <img src={ser5} alt="" />
                   Optimization IT consulting infrastructure
@@ -129,7 +132,7 @@ const Header = () => {
               spy={true}
               smooth={true}
               duration={500}
-              className="text-[14px] border-b-[3px] border-b-[#1b59f700] hover:border-b-[#1b5bf7] font-bold pb-2 cursor-pointer"
+              className="text-[14px] border-b-[3px] border-b-[#1b59f700]  hover:border-b-[#1b5bf7] font-bold pb-2 cursor-pointer"
             >
               Tools
             </Link>
@@ -169,7 +172,7 @@ const Header = () => {
                   smooth={true}
                   duration={500}
                   style={{ border: "none" }}
-                  className="text-[14px] px-8 border-b-[2px] border-b-[#1b59f700] hover:bg-[#1b5bf7] py-2 font-bold cursor-pointer flex items-center gap-4"
+                  className="text-[14px] px-8 border-b-[2px] border-b-[#1b59f700] hover:text-[#fff] hover:bg-[#1b5bf7] py-2 font-bold cursor-pointer flex items-center gap-4"
                 >
                   <img src={port1} alt="" />
                   Delever
@@ -181,7 +184,7 @@ const Header = () => {
                   smooth={true}
                   style={{ border: "none" }}
                   duration={500}
-                  className="text-[14px] px-8 border-b-[2px] border-b-[#1b59f700] hover:bg-[#1b5bf7] py-2 font-bold cursor-pointer flex items-center gap-4"
+                  className="text-[14px] px-8 border-b-[2px] border-b-[#1b59f700] hover:text-[#fff] hover:bg-[#1b5bf7] py-2 font-bold cursor-pointer flex items-center gap-4"
                 >
                   <img src={port2} alt="" />
                   Sms.uz
@@ -193,7 +196,7 @@ const Header = () => {
                   style={{ border: "none" }}
                   smooth={true}
                   duration={500}
-                  className="text-[14px] px-8 border-b-[2px] border-b-[#1b59f700] hover:bg-[#1b5bf7] py-2 font-bold cursor-pointer flex items-center gap-4"
+                  className="text-[14px] px-8 border-b-[2px] border-b-[#1b59f700] hover:text-[#fff] hover:bg-[#1b5bf7] py-2 font-bold cursor-pointer flex items-center gap-4"
                 >
                   <img src={port3} alt="" />
                   Goodzone
@@ -205,7 +208,7 @@ const Header = () => {
                   style={{ border: "none" }}
                   smooth={true}
                   duration={500}
-                  className="text-[14px] px-8 border-b-[2px] border-b-[#1b59f700] hover:bg-[#1b5bf7] py-2 font-bold cursor-pointer flex items-center gap-4"
+                  className="text-[14px] px-8 border-b-[2px] border-b-[#1b59f700] hover:text-[#fff] hover:bg-[#1b5bf7] py-2 font-bold cursor-pointer flex items-center gap-4"
                 >
                   <img src={port4} alt="" />
                   Iman
@@ -213,7 +216,7 @@ const Header = () => {
               </div>
             </li>
           </li>
-          <li>
+          <li className="group relative">
             <div className="">
               <Link
                 activeClass="active"
@@ -225,6 +228,34 @@ const Header = () => {
               >
                 Language
               </Link>
+              <li className="hidden group-hover:block w-[130px] absolute top-[30px] -left-5  bg-[#fff] ">
+                <div className="flex flex-col gap-2 py-1 ">
+                  <Link
+                    activeClass="active"
+                    to="/"
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                    style={{ border: "none" }}
+                    className="text-[14px] px-2 border-b-[2px] border-b-[#1b59f700] hover:text-[#fff] hover:bg-[#1b5bf7] py-2 font-bold cursor-pointer flex items-center gap-2"
+                  >
+                    <img src={rus} alt="Rus" className="w-[29px] h-[29px]" />
+                    Rus
+                  </Link>
+                  <Link
+                    activeClass="active"
+                    to="/"
+                    spy={true}
+                    smooth={true}
+                    style={{ border: "none" }}
+                    duration={500}
+                    className="text-[14px] px-2 border-b-[2px] border-b-[#1b59f700] hover:text-[#fff] hover:bg-[#1b5bf7] py-2 font-bold cursor-pointer flex items-center gap-2"
+                  >
+                    <img src={eng} alt="" className="w-[29px] h-[29px]" />
+                    Eng
+                  </Link>
+                </div>
+              </li>
             </div>
           </li>
           <button className="text-[16px] font-bold rounded-lg bg-[#1b5bf7] px-6 py-2 text-[#fff]">
